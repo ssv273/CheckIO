ed0c1ff8c46798de142b5b2cc2c8dd63786c6204
ssv273 <ssv273@mail.ru>
2018-10-24 16:34:44 +0700

    Я решил!!! Сам!!

--- try2.py
+++ try2.py
@@ -2,6 +2,7 @@
 
 
 text = input('String:')
+text = text.lower()  # переводим все символы в нижний регистр
 
 letter_list = []
 freq_list = []
@@ -15,10 +16,10 @@ if 0 < len(text) <= 10 ** 5:
     for symbol in text:
         if symbol.isalpha():
             letter_list += symbol
-    print(letter_list, len(letter_list))
+    # print(letter_list, len(letter_list))
 # считаем для списка из букв количесво повторений
     for freqency in letter_list:
-        freq_list += str(letter_list.count(freqency))
+        freq_list.append(letter_list.count(freqency))
     print(freq_list, len(freq_list))
 # определяем максимальное количество повторений
     for el in freq_list:
@@ -29,9 +30,10 @@ if 0 < len(text) <= 10 ** 5:
         if freq_number == max_freq:
             finish_list.append(letter_list[count])
         count += 1
-    print(finish_list)
+    # print(finish_list)
     # сортируем список
     finish_list.sort()
     # определяем искомую букву
     letter = finish_list[0]
     print(letter)
+    print(type(letter))
